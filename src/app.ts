0cbd95cb671ac3759d2f49c6118056516cba3cd6
MyPoZi <kxx555u.u@gmail.com>
2020-01-24 17:05:28 +0900

    add /tasks 追加

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,27 @@ import * as Express from "express"
 
 const app = Express()
 
+interface Task {
+    category: string
+    title: string
+    done: boolean
+}
+
+const tasks: Task[] = [
+    {
+        category: "Private",
+        title: "買い物",
+        done: false
+    }
+]
+
 app.get("/", (req, res) => {
-    res.send("Hello, VS Code!!!")
+    res.send("Hello, VS Code!!!!!")
 })
+
+app.get("/tasks", (req, res) => {
+    res.send(tasks)
+})
+
 
 export { app }
