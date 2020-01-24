eb240ea67b6296788c3fce9d0ae7bb667adb21d6
MyPoZi <kxx555u.u@gmail.com>
2020-01-24 17:32:50 +0900

    add postデータを受け取る

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import * as bodyParser from "body-parser"
 import * as Express from "express"
 
 const app = Express()
+app.use(bodyParser.json())
 
 interface Task {
     category: string
@@ -22,6 +24,21 @@ app.get("/", (req, res) => {
 
 app.get("/tasks", (req, res) => {
     res.send(tasks)
+})
+
+app.post("/tasks", (req, res) => {
+    const received = req.body
+    if ("category" in received && "title" in received && "done" in received) {
+        const newTask: Task = {
+            category: received.category,
+            title: received.title,
+            done: received
+        }
+        tasks.push(newTask)
+        console.log('Add:', newTask);
+    } else {
+        res.status(400).send("Parameters are invalid.")
+    }
 })
 
 
