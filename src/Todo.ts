f05cebba67a62f52d5c7526411b0c378c64739dd
MyPoZi <kxx555u.u@gmail.com>
2020-01-26 02:22:19 +0900

    add tslint入れたり、jestでテスト書いたり、インターフェイスを外に切り出したり

---
+++ src/Todo.ts
@@ -0,0 +1,5 @@
+export interface Todo {
+  category: string;
+  title: string;
+  done: boolean;
+}
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,39 @@
-import * as bodyParser from "body-parser"
-import * as Express from "express"
+import * as bodyParser from "body-parser";
+import * as Express from "express";
+import { Todo } from "./Todo.1";
 
-const app = Express()
-app.use(bodyParser.json())
+const express = Express();
+express.use(bodyParser.json());
 
-interface Task {
-    category: string
-    title: string
-    done: boolean
-}
+const tasks: Todo[] = [
+  {
+    category: "Private",
+    title: "買い物",
+    done: false
+  }
+];
 
-const tasks: Task[] = [
-    {
-        category: "Private",
-        title: "買い物",
-        done: false
-    }
-]
+express.get("/", (req, res) => {
+  res.send("Hello, VS Code!!!");
+});
 
-app.get("/", (req, res) => {
-    res.send("Hello, VS Code!!!")
-})
+express.get("/tasks", (req, res) => {
+  res.send(tasks);
+});
 
-app.get("/tasks", (req, res) => {
-    res.send(tasks)
-})
+express.post("/tasks", (req, res) => {
+  const received = req.body;
+  if ("category" in received && "title" in received && "done" in received) {
+    const newTask: Todo = {
+      category: received.category,
+      title: received.title,
+      done: received
+    };
+    tasks.push(newTask);
+    console.log("Add:", newTask);
+  } else {
+    res.status(400).send("Parameters are invalid.");
+  }
+});
 
-app.post("/tasks", (req, res) => {
-    const received = req.body
-    if ("category" in received && "title" in received && "done" in received) {
-        const newTask: Task = {
-            category: received.category,
-            title: received.title,
-            done: received
-        }
-        tasks.push(newTask)
-        console.log('Add:', newTask);
-    } else {
-        res.status(400).send("Parameters are invalid.")
-    }
-})
-
-
-export { app }
+export { express as app };
